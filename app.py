ae67b881da204f8dea84560e710d34415d14ccb3
hectorallardh-coder <hector.allardh@skola.taby.se>
2026-09-17 10:37:53 +0200

    Gjorde uppgift

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("om.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("kontakt.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
